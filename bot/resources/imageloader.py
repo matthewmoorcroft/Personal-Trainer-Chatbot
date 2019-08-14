7820a2d80a3c99f695c5e309e00ec003664f2d02
matthewmoorcroft <matthewmoorcroft@gmail.com>
2019-08-14 20:25:13 +0200

    fixed mageloader

--- bot/resources/imageloader.py
+++ bot/resources/imageloader.py
@@ -8,9 +8,8 @@ def get_photo():
     image_name = request.args['name']
     type = request.args['type']
 
-    return send_file(image_name, mimetype=('image/'+type)
+    return send_file(image_name, mimetype=('image/'+type))
 
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=5089, debug=True)
