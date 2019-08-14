fd45ec74518b46dd68241e1032d96f21c8bde76b
matthewmoorcroft <matthewmoorcroft@gmail.com>
2019-08-14 20:19:47 +0200

    fileloader

---
+++ bot/resources/imageloader.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/photo', methods=['GET'])
+def get_photo():
+    image_name = request.args['name']
+    type = request.args['type']
+
+    return send_file(image_name, mimetype=('image/'+type)
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5089, debug=True)
